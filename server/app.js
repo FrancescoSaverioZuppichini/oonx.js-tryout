f796d0e044031eb5b44b7213770aff8020fd4b1a
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2020-02-18 21:12:37 +0100

    first beta

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require("path")
+const logger = require("morgan")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+const PORT = 8080
+const app = express()
+app.use(cors())
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res, next) => {
+  res.send('Hi')
+})
+app.listen({ port: PORT }, () =>
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}`
+  )
+)
